d5dab393c3185cc98f88ed611a9b484b2871260c
Mudassir Khushik <150449494+MudassirKhushik@users.noreply.github.com>
2023-12-02 18:59:14 +0500

    Create code.cpp

---
+++ code.cpp
@@ -0,0 +1,20 @@
+//Math Algorithms
+#include <iostream>
+#include <cmath>
+
+using namespace std ;
+
+int main () {
+	
+	cout << pow (2, 5) << endl;
+	cout << pow (3, 4) << endl;
+	cout << sqrt (81) << endl; 
+	cout << round (4.6) << endl;
+	cout << ceil (5.1) << endl;
+	cout << floor (4.9) << endl; 
+	cout << fmax (20, 4) << endl;
+	cout << fmin (16, 8) << endl;
+	
+	
+	return 0;
+}
